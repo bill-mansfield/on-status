11f9f259f9ec28b11a90904e49150d1bb06809b3
Bill Mansfield <manbil03@gmail.com>
2021-08-24 20:43:26 +0930

    feature: laid down dev plan

--- src/api/main.go
+++ src/api/main.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 
 	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
@@ -46,7 +47,28 @@ func postData() {
 
 func main() {
 	// Get data from Open Weather
-	resp, err := http.Get("https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=-35.55&lon=138.2333&dt=1629597555&appid=9219e14c1a190adc052618d596aa7e28")
+
+	// The api url below contains the following prarams
+	// Latitude
+	// Longitude
+	// dt: unix time stamp from which to get weather data from the previous 24hrs
+	// Api key
+	// If we call this end point once a day we can collate the previous 24hrs of weather everyday
+	// Creating our own free historical weather data
+
+	// The data is returned in hourly blocks,
+	// We must add up rainfall if present
+	// We must average temp
+
+	// Step 1: Get data from Open Weather api on a daily basis via github action
+	// Step 2: Add up and average data -> save to DB
+	// Step 3: Read data from DB compare it to model for perfect conditions
+	// Step 4: If conditions are perfect notify discord
+
+	currentTime := time.Now().Unix()
+	fmt.Println("Current Unix Time:", currentTime)
+
+	resp, err := http.Get("https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=-35.55&lon=138.2333&dt=1629681702&appid=9219e14c1a190adc052618d596aa7e28")
 	if err != nil {
 		log.Fatalln(err)
 	}
